e85717e0b69dbcf682c47edaeb2da49f2d890f53
faizvk <faizvk14@gmail.com>
2025-08-29 01:21:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 //your JS code here. If required.
+const nameInput = document.getElementById("name");
+const ageInput = document.getElementById("age");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const name = nameInput.value.trim();
+  const age = parseInt(ageInput.value.trim());
+
+  if (name === "" || isNaN(age)) {
+    alert("Please enter valid details.");
+  } else {
+    const myPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (age >= 18) {
+          resolve(`Welcome, ${name}. You can vote.`);
+        } else {
+          reject(`Oh sorry ${name}. You aren't old enough.`);
+        }
+      }, 4000);
+    });
+
+    myPromise
+      .then((data) => {
+        alert(data);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  }
+});
